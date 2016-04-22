fb2e8dd84ed8f42ecd90617d92fa5f10055d0d83
Sebastian Laitila <sebastian.laitila@keskustekniikkaoy.local>
2016-04-22 19:42:19 +0300

    init

---
+++ henkiapp/www/js/constants.js
@@ -0,0 +1,11 @@
+angular.module('starter')
+ 
+.constant('AUTH_EVENTS', {
+  notAuthenticated: 'auth-not-authenticated'
+})
+ 
+.constant('API_ENDPOINT', {
+ //cls url: 'http://192.168.5.70:8080/api'
+ url: 'http://192.168.144.122:8080/api'
+  //  For a simulator use: url: 'http://127.0.0.1:8080/api'
+});
